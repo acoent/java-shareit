067b34c723002bbb6d0c46b00615bbdb9806cafa
acoent <153868119+acoent@users.noreply.github.com>
2025-09-28 07:21:45 -0500

    properties

--- gateway/src/main/java/ru/practicum/shareit/config/WebClientConfig.java
+++ gateway/src/main/java/ru/practicum/shareit/config/WebClientConfig.java
@@ -1,7 +1,10 @@
 package ru.practicum.shareit.config;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +12,9 @@ public class WebClientConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        CloseableHttpClient httpClient = HttpClients.custom().build();
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(requestFactory);
     }
 }
